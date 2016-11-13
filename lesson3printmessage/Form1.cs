930c8eca1f0ac69e2de269eb75355e997cec380d
Tom Owsiak <towsiak@yahoo.com>
2016-11-12 20:24:30 -0500

    lesson 3project

---
+++ lesson3printmessage/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lesson3printmessage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //msgLabel is the name of our label
+            //msgLabel.Text will allow us to set the text property
+            //= is the assignment operator
+            //"Hello, World" is a string
+            //; terminates the statement
+            msgLabel.Text = "Hello, World!";
+
+        }
+    }
+}
